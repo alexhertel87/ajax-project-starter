e76e5ed54add1d36873bed3c92085bf231bbe60b
Alex <alex.hertel87@gmail.com>
2021-04-08 15:09:52 -0500

    finished phase 3, midway thru phase 4

--- public/events.js
+++ public/events.js
@@ -1,20 +1,29 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const getKitties = async function () {
-        const response = await fetch("/kitten/image");
-        const json = await response.json();
-        if (response.ok) {
-            let catPic = document.querySelector(".cat-pic");
-            catPic.setAttribute("src", json.src);
-        } else {
-            addError(json);
-        }
-    }
-    getKitties();
+   let upVote = document.getElementById("upvote")
+   let downVote = document.getElementById("downvote")
+   let score = document.querySelector(".score")
+   let loading = document.querySelector(".loader");
 
-    let newPic = document.getElementById("new-pic");
-    let loading = document.querySelector(".loader");
-    newPic.addEventListener("click", (event) => {
-        loading.innerHTML = "Loading...";
-        getKitties();
-    })
+   const getKitties = async function () {
+      const response = await fetch("/kitten/image");
+      const json = await response.json();
+
+      if (response.ok) {
+         let catPic = document.querySelector(".cat-pic");
+         catPic.setAttribute("src", json.src);
+         loading.innerHTML = ""
+      } else {
+         window.alert("Something Went Wrong. Please Try Again.")
+      }
+   }
+   getKitties();
+
+   let newPic = document.getElementById("new-pic");
+   newPic.addEventListener("click", (event) => {
+      loading.innerHTML = "Loading...";
+      getKitties();
+   })
+   upVote.addEventListener("click", (e) => {
+      score.val
+   })
 })
